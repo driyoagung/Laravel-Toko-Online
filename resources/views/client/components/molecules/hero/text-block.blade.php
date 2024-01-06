275e8433e1aecb1021d00c55d349d6acd20fc2bb
driyoagung <dieoganda@gmail.com>
2024-01-06 21:11:12 +0700

    commit pertama

---
+++ resources/views/client/components/molecules/hero/text-block.blade.php
@@ -0,0 +1,8 @@
+@push('css')
+    <link rel="stylesheet" href="{{ asset('client/components/molecules/hero/text-block.css') }}">
+@endpush
+
+<div class="text-center text-block-hero py-5">
+    <h1>Sentuhan Unik, Gaya Jaya</h1>
+    <p>Selamat datang di toko fashion kami, di mana gaya dan kenyamanan bertemu dalam koleksi pakaian modis dan unik. Temukan elegansi sehari-hari dengan sentuhan desain yang istimewa, mencerminkan kepercayaan diri Anda. Berbelanja di sini untuk menemukan gaya yang memancarkan keindahan dan kepribadian Anda.</p>
+</div>
